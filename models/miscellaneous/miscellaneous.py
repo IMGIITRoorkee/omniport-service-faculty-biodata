0b5295c0d7503251a8959765fec5f8e73bbc1983
Aman Sharma <mannu.poski10@gmail.com>
2020-06-27 18:57:36 +0530

    Add constraints in the Miscellaneous model
    * Heading and faculty should be unique
    * Description is optional
    * Add plural name of the table

--- models/miscellaneous/miscellaneous.py
+++ models/miscellaneous/miscellaneous.py
@@ -4,33 +4,35 @@ from django.db import models
 from faculty_biodata.models.abstract_classes.base_model import BaseModel
 
 class AbstractMiscellaneous(BaseModel):
-	"""
-	This model contains the miscellaneous things a Faculty member wants to add
-	"""
+    """
+    This model contains the miscellaneous things a Faculty member wants to add
+    """
 
-	heading = models.CharField(
-		max_length=255,
-	)
+    heading = models.CharField(
+        max_length=255,
+    )
 
-	description = models.TextField()
+    description = models.TextField(
+        blank=True,
+    )
 
-	class Meta:
-		"""
-		Meta class for AbstractMiscellaneous
-		"""
+    class Meta:
+        """
+        Meta class for AbstractMiscellaneous
+        """
 
-		abstract = True
+        abstract = True
 
-	def __str__(self):
-		"""
-		Return the string representation of the model
-		:return: the string representation of the model
-		"""
+    def __str__(self):
+        """
+        Return the string representation of the model
+        :return: the string representation of the model
+        """
 
-		faculty_member = self.faculty_member
-		heading = self.heading
-		description = self.description
-		return f'{faculty_member}: {heading} ({description})'
+        faculty_member = self.faculty_member
+        heading = self.heading
+        description = self.description
+        return f'{faculty_member}: {heading} ({description})'
 
 
 class Miscellaneous(AbstractMiscellaneous):
@@ -44,3 +46,5 @@ class Miscellaneous(AbstractMiscellaneous):
         """
 
         swappable = swapper.swappable_setting('faculty_biodata', 'Miscellaneous')
+        unique_together = ['faculty_member', 'heading']
+        verbose_name_plural = 'miscellaneous'
