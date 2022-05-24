f9f738a9ba97a8c52687238948ee91df68b59873
Achintya Nath <achintya_n@cs.iitr.ac.in>
2022-05-24 13:35:25 +0530

    Add professional_background model

--- models/__init__.py
+++ models/__init__.py
@@ -31,6 +31,9 @@ from faculty_biodata.models.engagements.positions.membership import (
 from faculty_biodata.models.engagements.positions.administration import (
     AdministrativePosition,
 )
+from faculty_biodata.models.engagements.positions.professional import (
+    ProfessionalBackground,
+)
 
 # Events
 from faculty_biodata.models.events.event import (
